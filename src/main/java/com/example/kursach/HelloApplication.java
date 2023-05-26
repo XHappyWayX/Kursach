d8fa5be56302875028ae7a48665c5a98c68077a6
HappyWay <hhappywayy@gmail.com>
2023-05-26 19:39:00 +0300

    0.3(rent coast)

--- src/main/java/com/example/kursach/HelloApplication.java
+++ src/main/java/com/example/kursach/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 630, 350);
         stage.setTitle("Car Rent Admin Panel");
         stage.setScene(scene);
         stage.setResizable(false);
